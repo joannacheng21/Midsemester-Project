07bf80169e87648f91f8059e90f157a050f4f5ef
Joanna Cheng <joannacheng21@outlook.com>
2022-10-03 16:43:38 -0400

    javascript testing

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 import fetch from "node-fetch";
 
-fetch("https://pokeapi.co/api/v2/pokemon/1")
+let currPokemon = 2;
+
+fetch("https://pokeapi.co/api/v2/pokemon/")
     .then((resp) => resp.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        const pokemonData = data;
+        console.log(pokemonData["name"]);
+        console.log(pokemonData["types"][0]["type"]["name"]);
+        if (pokemonData["types"].length == 2)
+            console.log(pokemonData["types"][1]["type"]["name"]);
+        console.log("height: " + pokemonData["height"]/10.0 + "m");
+        console.log("weight: " + pokemonData["weight"]/100.0 + "kg");
+        pokemonData["stats"].forEach(e => console.log(e["stat"]["name"] + ": " + e["base_stat"]));
+
+        const textbox = document.getElementById("nametext");
+        console.log(textbox);
+        document.getElementById("nametext").textContent = "asdfasdf";
+    });
 
 //there are 905 pokemon
+
+//height is divided by 10
+//weight is divided by 100
