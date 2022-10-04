cb678b704f3524a18a71a099cd9564b1c2b2cbbb
Joanna Cheng <joannacheng21@outlook.com>
2022-10-04 19:59:14 -0400

    javascript and small features

--- script.js
+++ script.js
@@ -1,23 +1,80 @@
-import fetch from "node-fetch";
+fetchPokemonData(1);
 
-let currPokemon = 2;
+const totalPokemon = 905;
+let currPokemon = 1;
+const typeColors = {"normal":"#A8A77A", "fire":"#EE8130", "water":"#6390F0", "electric":"#F7D02C", "grass":"#7AC74C", "ice":"#96D9D6", "fighting":"#C22E28", "poison":"#A33EA1", "ground":"#E2BF65", "flying":"#A98FF3", "psychic":"#F95587", "bug":"#A6B91A", "rock":"#B6A136", "ghost":"#735797", "dragon":"#6F35FC", "dark":"#705746", "steel":"#B7B7CE", "fairy":"#D685AD"};
 
-fetch("https://pokeapi.co/api/v2/pokemon/")
+let infomoves = [];
+
+leftBtn = document.getElementById("lbutton");
+rightBtn = document.getElementById("rbutton");
+leftBtn.addEventListener("click", (e) => {
+    currPokemon -= 1;
+    if (currPokemon < 1)
+        currPokemon = totalPokemon;
+    fetchPokemonData(currPokemon);
+});
+rightBtn.addEventListener("click", (e) => {
+    currPokemon += 1;
+    if (currPokemon > 905)
+        currPokemon = 1;
+    fetchPokemonData(currPokemon);
+});
+
+infoBtn = document.getElementById("infobtn");
+movesBtn = document.getElementById("movesbtn");
+infoBtn.addEventListener("click", (e) => {
+    //set info
+    document.getElementById("infotext").textContent = infomoves[0];
+    document.getElementById("infobtn").style.backgroundColor = "#7CFF79";
+    document.getElementById("movesbtn").style.backgroundColor = "#E8E8E8";
+});
+movesBtn.addEventListener("click", (e) => {
+    //set moves
+    document.getElementById("infotext").textContent = infomoves[1];
+    document.getElementById("infobtn").style.backgroundColor = "#E8E8E8";
+    document.getElementById("movesbtn").style.backgroundColor = "#7CFF79";
+});
+
+function fetchPokemonData(pokemonIndex) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemonIndex)
     .then((resp) => resp.json())
     .then((data) => {
-        const pokemonData = data;
-        console.log(pokemonData["name"]);
-        console.log(pokemonData["types"][0]["type"]["name"]);
-        if (pokemonData["types"].length == 2)
-            console.log(pokemonData["types"][1]["type"]["name"]);
-        console.log("height: " + pokemonData["height"]/10.0 + "m");
-        console.log("weight: " + pokemonData["weight"]/100.0 + "kg");
-        pokemonData["stats"].forEach(e => console.log(e["stat"]["name"] + ": " + e["base_stat"]));
+        //set image
+        document.getElementById("picture").src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + currPokemon + ".png";
 
-        const textbox = document.getElementById("nametext");
-        console.log(textbox);
-        document.getElementById("nametext").textContent = "asdfasdf";
+        //set name
+        document.getElementById("nametext").textContent = data["name"];
+
+        //set types
+        let t1 = data["types"][0]["type"]["name"];
+        document.getElementById("type1").textContent = t1;
+        document.getElementById("type1").style.backgroundColor = typeColors[t1];
+        if (data["types"].length == 2) {
+            let t2 = data["types"][1]["type"]["name"];
+            document.getElementById("type2").textContent = t2;
+            document.getElementById("type2").style.backgroundColor = typeColors[t2];
+        }
+        else {
+            document.getElementById("type2").textContent = "";
+            document.getElementById("type2").style.backgroundColor = "#FFFFFF";
+        }
+
+        //set info
+        let stats = "";
+        stats += "height: " + data["height"]/10.0 + "m\r\n"
+        stats += "weight: " + data["weight"]/100.0 + "kg\r\n"
+        data["stats"].forEach(e => stats += e["stat"]["name"] + ": " + e["base_stat"] + "\r\n");
+        const infoTextbox = document.getElementById("infotext");
+        infoTextbox.setAttribute('style', 'white-space: pre;');
+        infoTextbox.textContent = stats;
+
+        //save moves
+        moves = "";
+        data["moves"].forEach(e => moves += e["move"]["name"] + "\r\n");
+        infomoves = [stats, moves];
     });
+}
 
 //there are 905 pokemon
 
